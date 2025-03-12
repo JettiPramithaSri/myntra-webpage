c792dc7049cf28896679a5cf8645da2f1c3b2043
pramithasri <pramithasri.jetti@gmail.com>
2025-03-12 11:32:41 +0530

    myntra-website

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const shopNowButton = document.querySelector("button");
+    shopNowButton.addEventListener("click", function() {
+        alert("Redirecting to the shopping page...");
+    });
+});
